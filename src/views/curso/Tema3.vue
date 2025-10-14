99cdb7182f9bfde0713e22a8d8d2f617fbf57668
Francisco Lizcano <flizcanor@icloud.com>
2025-10-13 21:36:19 -0500

    Update Tema3.vue

--- src/views/curso/Tema3.vue
+++ src/views/curso/Tema3.vue
@@ -241,7 +241,7 @@
       .p-4.p-md-5
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/9cqLwqWDCzs?si=F1b2M6SlLRnGqFf9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/2KyWoTbb_a0?si=PBxsgf7Nq5dr3wxh" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 
 
